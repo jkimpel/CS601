bb56e2f35654f021551ec56f57a086fc7e7d6eeb
jkimpel <jkimpel@gmail.com>
2012-06-24 12:46:12 -0400

    Incremental Changes

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Script.js
+ * Joe Kimpel
+ * Homework 2
+ * CS 601
+ * 6.24.12
+ */
+
+function validate(){
+	alert("Looks Good!");
+}
---
+++ scriptHw2p1.js
@@ -0,0 +1,10 @@
+/* Script.js
+ * Joe Kimpel
+ * Homework 2 part 1
+ * CS 601
+ * 6.24.12
+ */
+alert("Welcome to my JavaScript Program!");
+str = prompt("What is your name?", "John Doe");
+alert("Your name is "+str);
+alert("Thanks for using my program!");
